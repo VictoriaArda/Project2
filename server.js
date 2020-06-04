b93e844c9c450ffda2b02d8ff6726350f4288103
VictoriaArda <ardavictoria@gmail.com>
2020-06-04 18:56:32 -0400

    sql

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+/* eslint-disable quotes */
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
@@ -24,7 +26,7 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  });
+    app.listen(PORT, function() {
+        console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    });
 });
