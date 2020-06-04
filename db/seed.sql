a5b7b6c828d3e0c208832ee9a6938a1f22ca3272
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-06-04 19:29:20 -0400

    Changed to syntax

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,5 @@
 USE loginDB;
 INSERT INTO users (email, password)
-VALUES (ncrutgers@gmail.com, Mypassword),
-(ardavictoria@gmail.com, Mypassword2),
-(vladajka16@gmail.com, Mypassword3);
+VALUES ("ncrutgers@gmail.com", "Mypassword"),
+("ardavictoria@gmail.com", "Mypassword2"),
+("vladajka16@gmail.com", "Mypassword3");
