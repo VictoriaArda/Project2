e7acb04d0cbd760cf00850ce63a14505222f9100
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-06-04 21:38:05 -0400

    Changes to schema and seed file

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,3 @@
 -- Creates login database
 DROP DATABASE IF EXISTS loginDB;
 CREATE DATABASE loginDB;
-USE loginDB;
--- Creates table
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-);
